01f751dcb16a4a8bbb46e13747810c71a1992780
Валерия Чукович <valeriachukovich@Valerias-MacBook-Pro.local>
2020-04-30 20:59:52 +0200

    that is all

--- js/script.js
+++ js/script.js
@@ -3,5 +3,30 @@ $(document).ready(function() {
         $('.header__burger, .menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 1000,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 1190,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 740,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+    });
 });
       
